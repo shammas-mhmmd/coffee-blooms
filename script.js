16fed60f55f39585249adbac2c89148f0f15b093
Shammas Muhammad <muhammedshammas9697@gmail.com>
2025-12-15 20:13:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-// Future enhancements can be added here
-console.log("Coffee Blooms Homestay loaded");
+
+// LIGHTBOX GALLERY
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const images = document.querySelectorAll(".lightbox-img");
+const closeBtn = document.querySelector(".lightbox-close");
+
+images.forEach(img => {
+  img.addEventListener("click", () => {
+    lightbox.style.display = "flex";
+    lightboxImg.src = img.src;
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
+
+lightbox.addEventListener("click", (e) => {
+  if (e.target === lightbox) {
+    lightbox.style.display = "none";
+  }
+});
